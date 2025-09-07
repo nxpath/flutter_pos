b405bb770200415d5d5f06e3f457211a634e9b42
nxpath <96268344+nxpath@users.noreply.github.com>
2025-09-08 00:17:20 +0700

    Create firebase_options.dart

---
+++ lib/firebase_options.dart
@@ -0,0 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+
+class DefaultFirebaseOptions {
+  static const FirebaseOptions currentPlatform = FirebaseOptions(
+    apiKey: 'AIzaSyBNBtf5v-pe_X44RAzg10Nyk4sbApXJBlk',
+    appId: '1:1053707840321:android:540d8c603d82860d03cfc0',
+    messagingSenderId: '1053707840321',
+    projectId: 'flutterpos-68386',
+    storageBucket: 'flutterpos-68386.firebasestorage.app',
+  );
+}
