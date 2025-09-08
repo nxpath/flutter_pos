8f6c55c34383f8d4b5e6019e869ef8614211008a
nxpath <96268344+nxpath@users.noreply.github.com>
2025-09-08 13:46:51 +0700

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
 
 class DefaultFirebaseOptions {
   static const FirebaseOptions currentPlatform = FirebaseOptions(
